41ead59496727f0ecab14aa26ac17b60905c1cda
Shahrza <gshahrza@gmail.com>
2022-04-26 12:20:07 +0400

    Mapper Interface added

--- src/main/java/az/iktlab/securityservice/mapper/AccountMapper.java
+++ src/main/java/az/iktlab/securityservice/mapper/AccountMapper.java
@@ -3,7 +3,11 @@ package az.iktlab.securityservice.mapper;
 import az.iktlab.securityservice.dao.entity.Account;
 import az.iktlab.securityservice.dto.request.AccountRequestDTO;
 import az.iktlab.securityservice.dto.response.AccountResponseDTO;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Mapper(componentModel = "spring")
+@Component
 public interface AccountMapper {
 
     Account accountRequestDTOtoAccount(AccountRequestDTO requestDTO);
--- src/main/java/az/iktlab/securityservice/mapper/RoleMapper.java
+++ src/main/java/az/iktlab/securityservice/mapper/RoleMapper.java
@@ -2,7 +2,10 @@ package az.iktlab.securityservice.mapper;
 
 import az.iktlab.securityservice.dao.entity.Role;
 import az.iktlab.securityservice.dto.response.RoleResponseDTO;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Mapper(componentModel = "spring")
 public interface RoleMapper {
 
     RoleResponseDTO roleToRoleResponseDTO(Role role);
